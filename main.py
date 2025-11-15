be5ac1a20fe82053d0e35292aed94424d3020126
lamar <setemikay@gmail.com>
2025-11-15 07:46:03 +0100

    survey

---
+++ main.py
@@ -0,0 +1,7 @@
+#ask the user to pick a number between 1-10
+name=input("Enter a name:")
+number=input("Enter a number between 1-10:")
+print(f"Hello {name} thanks for choosing number {number} i really like that number :)")
+print(f"hello {name} how ya doing")
+feedback=input("enter how u feel right now")
+print(f"thanks for saying ur{feedback}")
