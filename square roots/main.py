3acd999619704559e6be87342f45c8c8e2606423
lamar <setemikay@gmail.com>
2025-11-17 20:47:57 +0100

    yay

---
+++ square roots/main.py
@@ -0,0 +1,5 @@
+#ask the user to input a number
+number=float(input("Enter a number"))
+#determine the square root of that number
+square_root=number ** 0.5
+print(f"The square root of {number} is {square_root}")
